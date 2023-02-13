0642eb43a91b94334bfff0e5d310b80ae4417360
Saiful Islam <saifulprogrammer@gmail.com>
2023-02-14 01:15:59 +0600

    refactor

---
+++ ToDo.Api/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDo.Application.IService;
+using ToDo.Application.Response;
+using ToDo.Domain.DBModel;
+using ToDo.Domain.Enum;
+using ToDo.Domain.IEntity;
+using ToDo.Domain.Model;
+
+namespace ToDo.Api.Controllers
+{
+    [Authorize]
+    public class UserController : BaseController
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserService _userService;
+        private readonly ICurrentUser _currentUser;
+        private readonly IMapper _mapper;
+        public UserController(UserManager<ApplicationUser> userManager, ICurrentUser currentUser)
+        {
+            _userManager = userManager;
+            _currentUser = currentUser;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("GetUsers")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var data = await _userManager.Users.ToListAsync();
+            var users = data.Select(x => new
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Email = x.Email
+            });
+            
+            return Ok(users);
+        }
+        
+        [HttpPatch]
+        [Route("ChangePassword")]
+        public async Task<IActionResult> ChangePassword(ChangePasswordModel changePasswordModel)
+        {
+            var user = await _userManager.FindByIdAsync(_currentUser.User.Id.ToString());
+            await _userManager.ChangePasswordAsync(user, changePasswordModel.CurrentPassword, changePasswordModel.NewPassword);
+            return Ok();
+        }
+    }
+}
---
+++ ToDo.Domain/Model/ChangePasswordModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Domain.Model
+{
+    public class ChangePasswordModel
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
